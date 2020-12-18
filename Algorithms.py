65427f581935c0e18a2844acc16648bff8271533
waseem tannous <waseemt99@gmail.com>
2020-12-18 15:47:43 +0200

    initializing Algorithms.py

---
+++ Algorithms.py
@@ -0,0 +1,26 @@
+def astar():
+    pass
+
+
+def ids():
+    pass
+
+
+def ucs():
+    pass
+
+
+def idAstar():
+    pass
+
+
+def biAstar():
+    pass
+
+
+def heuristic1():
+    pass
+
+
+def heuristic2():
+    pass
