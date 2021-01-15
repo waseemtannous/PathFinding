9dbe9d379257cd847085f52224122a2ea4382473
saji- <s.a.j.i-10@hotmail.com>
2021-01-16 00:18:16 +0200

    Documentation

--- BIASTAR.py
+++ BIASTAR.py
@@ -48,24 +48,30 @@ def biAstar(maze):
     # maze.max_time = math.sqrt(maze.size)
     maze.max_time = 1000
     time_start = time.time()
+    # Min heap for choosing the minimum cost every time.
     open_heap_start = []
     open_heap_end = []
 
+    # Two hash tabels for each direction, open list (neighbors of the expanded node)
+    # and close list (contains expanded nodes) ,O(1) time complixty for finding an item in the table
     open_dictionary_start = {}
     open_dictionary_end = {}
 
     closed_dictionary_start = {}
     closed_dictionary_end = {}
 
+    # Getting the coordinates of start node and the goal node
     x1, y1 = maze.get_start()
     x2, y2 = maze.get_end()
-
+    # Two grids, THE FIRST GRID WE RUN A star from the start
+    # the second we run from the goal node
     grid = maze.get_grid()
     second_grid = maze.get_second_grid()
-
+    # Getting start node and the goal node
     start_node = grid[x1][y1]
     end_node = second_grid[x2][y2]
-
+    # For both of them We calculate the F value for the node
+    # Push the start node to the min heap and put in the open list (open_dictionary)
     calculate_f_cost(maze, node=start_node, end=end_node)
     heapq.heappush(open_heap_start, start_node)
     open_dictionary_start[start_node] = True
@@ -81,10 +87,12 @@ def biAstar(maze):
 
     previous_node_start = None
     previous_node_end = None
-
+    # While the open list is not empty and we are not out of time continue
     while len(open_heap_start) != 0 and len(open_heap_end) and (time.time() - time_start <= maze.max_time):
+        # We get the node with minimal cost
         current_node_start = heapq.heappop(open_heap_start)
         current_node_start.make_closed()
+        # Remove the node from the open list ,and add it to the close list
         open_dictionary_start[current_node_start] = False
         closed_dictionary_start[current_node_start] = True
         # draw_node(maze, current_node_start)
@@ -94,9 +102,9 @@ def biAstar(maze):
                 maze.update_cuttoff(previous_node_start.depth)
 
         previous_node_start = current_node_start
-
+        # Getting the current node on the second grid
         temp1 = second_grid[current_node_start.get_x()][current_node_start.get_y()]
-
+        # If the node is on close list from both sides ENTER HERE
         if closed_dictionary_end.get(temp1, False):
             temp1 = bidirectional_meeting_point(maze, temp1, open_heap_start, open_heap_end, open_dictionary_start,
                                                 open_dictionary_end,
@@ -104,6 +112,7 @@ def biAstar(maze):
             maze.actual_time = time.time() - time_start
             temp1.make_grey()
             draw_node(maze, temp1)
+            # Make path
             recreate_bidirectional_path(maze, temp1, came_from_start, came_from_end)
             return True
         maze.update_expanded_nodes()
@@ -111,9 +120,11 @@ def biAstar(maze):
                        came_from_start, open_heap_start)
 
         #   second one #######################
+        # We get the node with minimal cost
         current_node_end = heapq.heappop(open_heap_end)
         current_node_end.make_closed()
         current_node_end.make_blue()
+        # Remove the node from the open list ,and add it to the close list
         open_dictionary_end[current_node_end] = False
         closed_dictionary_end[current_node_end] = True
         # draw_node(maze, current_node_end)
@@ -123,9 +134,9 @@ def biAstar(maze):
                 maze.update_cuttoff(previous_node_end.depth)
 
         previous_node_end = current_node_end
-
+        # Getting the current node on the second grid
         temp2 = grid[current_node_end.get_x()][current_node_end.get_y()]
-
+        # If the node is on close list from both sides ENTER HERE
         if closed_dictionary_start.get(temp2, False):
             temp2 = bidirectional_meeting_point(maze, temp2, open_heap_start, open_heap_end, open_dictionary_start,
                                                 open_dictionary_end,
@@ -133,6 +144,7 @@ def biAstar(maze):
             maze.actual_time = time.time() - time_start
             temp2.make_grey()
             draw_node(maze, temp2)
+            # Make path
             recreate_bidirectional_path(maze, temp2, came_from_start, came_from_end)
             return True
         maze.update_expanded_nodes()
@@ -143,12 +155,17 @@ def biAstar(maze):
 
 
 def biAstar_helper(maze, current_node, end_node, open_dictionary, closed_dictionary, came_from, open_heap):
+    # Expanding the node and getting its neighbors
     neighbors = current_node.get_neighbors()
     for neighbor in neighbors:
+        # Updating the cost
         neighbor_current_cost = current_node.get_g() + neighbor.get_cost()
+        # If the node is already on the open list ENTER HERE
         if open_dictionary.get(neighbor, False):
+            # If the cost is larger then do nothing
             if neighbor.get_g() <= neighbor_current_cost:
                 continue
+            # Else if the cost is smaller update the node with the new one
             closed_dictionary[neighbor] = False
             neighbor.set_g(neighbor_current_cost)
             neighbor.set_depth(current_node.get_depth())
@@ -157,9 +174,12 @@ def biAstar_helper(maze, current_node, end_node, open_dictionary, closed_diction
             heapq.heapify(open_heap)
             neighbor.make_open()
             # draw_node(maze, neighbor)
+        # If the node is on the close list ENTER HERE
         elif closed_dictionary.get(neighbor, False):
+            # If the cost is larger then do nothing
             if neighbor.get_g() <= neighbor_current_cost:
                 continue
+            # Else if the cost is smaller update the node with the new one and move it to the open list
             closed_dictionary[neighbor] = False
             neighbor.set_g(neighbor_current_cost)
             neighbor.set_depth(current_node.get_depth())
@@ -170,6 +190,7 @@ def biAstar_helper(maze, current_node, end_node, open_dictionary, closed_diction
             open_dictionary[neighbor] = True
             # draw_node(maze, neighbor)
         else:
+            # Else add the node to open list
             neighbor.set_g(neighbor_current_cost)
             neighbor.set_depth(current_node.get_depth())
             came_from[neighbor] = current_node
@@ -180,7 +201,7 @@ def biAstar_helper(maze, current_node, end_node, open_dictionary, closed_diction
             closed_dictionary[neighbor] = False
             # draw_node(maze, neighbor)
 
-
+# This function tries to improve meeting point
 def bidirectional_meeting_point(maze, temp, open_heap_start, open_heap_end, open_dictionary_start, open_dictionary_end,
                                 closed_dictionary_start, closed_dictionary_end):
     x, y = temp.get_x(), temp.get_y()
@@ -192,6 +213,7 @@ def bidirectional_meeting_point(maze, temp, open_heap_start, open_heap_end, open
         current_node_end = maze.get_second_grid()[current_node_start.get_x()][current_node_start.get_y()]
         if open_dictionary_end.get(current_node_end, False) or closed_dictionary_end.get(current_node_end, False):
             temp_min_f = current_node_start.get_f() + current_node_end.get_f()
+            # Update the meeting point
             if temp_min_f < min_sum_f:
                 min_sum_f = temp_min_f
                 min_node = current_node_start
@@ -201,6 +223,7 @@ def bidirectional_meeting_point(maze, temp, open_heap_start, open_heap_end, open
         current_node_start = maze.get_grid()[current_node_end.get_x()][current_node_end.get_y()]
         if open_dictionary_start.get(current_node_start, False) or closed_dictionary_start.get(current_node_start, False):
             temp_min_f = current_node_end.get_f() + current_node_start.get_f()
+            # Update the meeting point
             if temp_min_f < min_sum_f:
                 min_sum_f = temp_min_f
                 min_node = current_node_end
@@ -214,7 +237,7 @@ def recreate_bidirectional_path(maze, node, came_from_start, came_from_end):
     maze.get_path().append(node)
     node.make_path()
     draw_node(maze, node)
-
+    #Get the parents on the end side
     while came_from_end.get(node, False):
         node = came_from_end[node]
         node.make_path()
@@ -224,7 +247,7 @@ def recreate_bidirectional_path(maze, node, came_from_start, came_from_end):
 
     maze.get_path().reverse()
     node = maze.get_grid()[node_x][node_y]
-
+    # Get the parents on the start side
     while node in came_from_start:
         node = came_from_start[node]
         node.make_path()
--- UCS.py
+++ UCS.py
@@ -45,33 +45,42 @@ def draw_node(maze, node):
     draw_grid(maze=maze)
     pygame.display.update()
 
-
+# We used heuristic h=0 because the implementation is the same as A star
 def ucs(maze):
     # maze.max_time = math.sqrt(maze.size)
     maze.max_time = 1000
     time_start = time.time()
+
+    # Min heap for choosing the minimum cost every time.
     open_heap = []
 
+    # Two hash tabels, open list (neighbors of the expanded node) and close list (contains expanded nodes) ,
+    # O(1) time complixty for finding an item in the table
     open_dictionary = {}
     closed_dictionary = {}
 
+    # Getting the coordinates of start node and the goal node
     x1, y1 = maze.get_start()
     x2, y2 = maze.get_end()
-
+    # Getting start node and the goal node
     start_node = maze.get_grid()[x1][y1]
     end_node = maze.get_grid()[x2][y2]
 
+
     start_node.set_h(0)
     start_node.calculate_f()
+    # We push the start node to the min heap and we put in the open list (open_dictionary)
     heapq.heappush(open_heap, start_node)
     open_dictionary[start_node] = True
 
     previous_node = None
-
+    # While the open list is not empty and we are not out of time continue
     while len(open_heap) != 0 and (time.time() - time_start <= maze.max_time):
+        # We get the node with minimal cost
         current_node = heapq.heappop(open_heap)
         maze.update_expanded_nodes()
         current_node.make_closed()
+        # Remove the node from the open list ,and add it to the close list
         open_dictionary[current_node] = False
         closed_dictionary[current_node] = True
         # draw_node(maze, current_node)
@@ -81,23 +90,29 @@ def ucs(maze):
                 maze.update_cuttoff(previous_node.depth)
 
         previous_node = current_node
-
+        # The GOAL node has been founded ENTER HERE!
         if current_node.get_x() == end_node.get_x() and current_node.get_y() == end_node.get_y():
             maze.actual_time = time.time() - time_start
+            # While the nodes are not the starting (that means they have a parent) point continue
             while current_node.get_parent() != None:
+                # Add the node to the path
                 maze.get_path().append(current_node)
                 current_node.make_path()
                 draw_node(maze, current_node)
                 current_node = current_node.get_parent()
             maze.get_path().append(start_node)
             return True
-
+        # Expanding the node and getting its neighbors
         neighbors = current_node.get_neighbors()
         for neighbor in neighbors:
+            # Updating the cost
             neighbor_current_cost = current_node.get_g() + neighbor.get_cost()
+            # If the node is already on the open list ENTER HERE
             if open_dictionary.get(neighbor, False):
+                # If the cost is larger then do nothing
                 if neighbor.get_g() <= neighbor_current_cost:
                     continue
+                # Else if the cost is smaller update the node with the new one
                 neighbor.set_g(neighbor_current_cost)
                 neighbor.set_depth(current_node.get_depth())
                 neighbor.set_parent(current_node)
@@ -106,9 +121,12 @@ def ucs(maze):
                 heapq.heapify(open_heap)
                 neighbor.make_open()
                 # draw_node(maze, neighbor)
+            # If the node is on the close list ENTER HERE
             elif closed_dictionary.get(neighbor, False):
+                # If the cost is larger then do nothing
                 if neighbor.get_g() <= neighbor_current_cost:
                     continue
+                # Else if the cost is smaller update the node with the new one and move it to the open list
                 closed_dictionary[neighbor] = False
                 neighbor.set_g(neighbor_current_cost)
                 neighbor.set_depth(current_node.get_depth())
@@ -120,6 +138,7 @@ def ucs(maze):
                 open_dictionary[neighbor] = True
                 # draw_node(maze, neighbor)
             else:
+                # Else add the node to open list
                 neighbor.set_g(neighbor_current_cost)
                 neighbor.set_depth(current_node.get_depth())
                 neighbor.set_parent(current_node)
